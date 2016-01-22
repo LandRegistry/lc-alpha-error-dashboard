2fa04133d37ac3b26829d4f1a1b22497c9ab2c4a
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-01-22 07:53:52 +0000

    Make consistent with the other legacy adapter; also check format of error objects

--- application/listener.py
+++ application/listener.py
@@ -2,15 +2,40 @@ import logging
 import requests
 import json
 from application.routes import app
+from jsonschema import validate, ValidationError
+
+
+ERROR_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "type": {"type": "string"},
+        "message": {"type": "string"},
+        "stack": {"type": "string"},
+        "subsystem": {"type": "string"}
+    },
+    "required": ["type", "message", "subsystem"]
+}
 
 
 def message_received(body, message):
     logging.info("Received new error: %s", str(body))
 
     # TODO: make sure it's right format for sending on...
+    try:
+        validate(body, ERROR_SCHEMA)
+        error = body
+        logging.info("Received error '{}' from {}".format(body['message'], body['subsystem']))
+    except ValidationError:
+        error = {
+            "type": "E",
+            "message": "Unknown error: " + json.dumps(body),
+            "subsystem": "unknown",
+            "stack": ""
+        }
+        logging.info("Received unknown error '{}'".format(json.dumps(body)))
+
     request_uri = app.config['LEGACY_DB_URI'] + '/errors'
-    logging.info('Blah')
-    response = requests.post(request_uri, data=json.dumps(body), headers={'Content-Type': 'application/json'})
+    response = requests.post(request_uri, data=json.dumps(error), headers={'Content-Type': 'application/json'})
     logging.info('POST to /errors: %s %s', response.status_code, response.reason)
     message.ack()
 
